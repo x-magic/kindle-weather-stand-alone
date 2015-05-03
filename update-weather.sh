d5d42213a24af2eaeee543a253565145d2a43b0f
Bill Haofei Gong <xmagic5589@gmail.com>
2015-05-03 19:17:26 +1000

    Added an Internet connection check script so Kindle will display an alert image when disconnected from Internet.

--- update-weather.sh
+++ update-weather.sh
@@ -15,26 +15,46 @@ BATTERY=`grep -o "[0-9]*" /sys/devices/system/yoshi_battery/yoshi_battery0/batte
 CURRENT=`cat /sys/devices/system/yoshi_battery/yoshi_battery0/battery_current`
 
 if [ $BATTERY -le 10 ] && [ $CURRENT -le 0 ]; then
-	if [ -e drained ]; then
-		# If already drawn drained image, then just leave. 
-		exit 0;
-	else
-		touch drained
-		# If device just got low power, then draw drained image. 
-		eips -c
-		eips -c
-		eips -g battery-drained.png
-	fi
+    if [ -e drained ]; then
+        # If already drawn drained image, then just leave. 
+        exit 0;
+    else
+        touch drained
+        # If device just got low power, then draw drained image. 
+        eips -c
+        eips -c
+        eips -g battery-drained.png
+    fi
 else
-	# Delete previously added flag
-	if [ -e drained ]; then rm -f drained; fi
-	# Disable screensaver
-	lipc-set-prop com.lab126.powerd preventScreenSaver 1
+    # Delete previously added flag
+    if [ -e drained ]; then rm -f drained; fi
+    # Disable screensaver
+    lipc-set-prop com.lab126.powerd preventScreenSaver 1
     # Get rid of old file first
     rm -f /tmp/crushed_weather.png
-    if ./local/generate_png ; then
-        eips -g /tmp/crushed_weather.png
+    # Enable Wi-Fi
+    if [ -e wireless ]; then lipc-set-prop com.lab126.cmd wirelessEnable 1; fi
+    # Loop test Internet connectivity
+    INTERNETCOUNT=5
+    while [ $INTERNETCOUNT -ne 0 ]; do
+        ping -c 1 8.8.8.8
+        PINGRESULT=$?
+        if [ $PINGRESULT -eq 0 ]; then INTERNETCOUNT=1; fi
+        INTERNETCOUNT=$(expr $INTERNETCOUNT - 1)
+    done
+    # Clear up the display
+    eips -c
+    eips -c
+    if [ $PINGRESULT -eq 0 ]; then
+        # Finally, let's get data and refresh
+        if ./local/generate_png ; then
+            eips -g /tmp/crushed_weather.png
+        else
+            eips -g weather-error.png
+        fi
     else
-        eips -g weather-error.png
+        eips -g internet-error.png
     fi
+    # Disable Wi-Fi
+    if [ -e wireless ]; then lipc-set-prop com.lab126.cmd wirelessEnable 0; fi
 fi
